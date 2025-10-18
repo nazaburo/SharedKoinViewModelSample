460b454acfd329d102eb1428df6dcfa7c4e6cf47
nazaburo <84699252+nazaburo@users.noreply.github.com>
2025-10-18 11:23:03 +0900

    Set up Koin for dependency injection and initialize application class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    // Koin
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/github/nazaburo/sharedkoinviewmodelsample/di/AppModule.kt
@@ -0,0 +1,6 @@
+package com.github.nazaburo.sharedkoinviewmodelsample.di
+
+import org.koin.dsl.module
+
+val appModule = module {
+}
